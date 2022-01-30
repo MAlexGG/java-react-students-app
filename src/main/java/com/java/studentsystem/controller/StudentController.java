10f42c2a8d83867d0c3bb65a1a45e9308fe1a963
MAGG <alexandragalarzag@gmail.com>
2022-01-30 15:17:03 +0100

    Api in Java with create and getAll methods

---
+++ src/main/java/com/java/studentsystem/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.java.studentsystem.controller;
+
+
+import com.java.studentsystem.model.Student;
+import com.java.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/new-student")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student added";
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+}
---
+++ src/main/java/com/java/studentsystem/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.java.studentsystem.service;
+
+import com.java.studentsystem.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+}
